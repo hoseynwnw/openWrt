23936f38eb544f07b0b5114df8161e6665de8416
hoseynwnw <35378226+hoseynwnw@users.noreply.github.com>
2022-05-30 00:01:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+#增加oaf?
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
